8a93ff458150b05202f4bf76ca06186a1c572f93
Raymond Chandler III <raymondchandleriii@gmail.com>
2014-07-15 15:50:42 -0400

    .

--- hpitclient/settings.py
+++ hpitclient/settings.py
@@ -3,9 +3,13 @@ class HpitClientSettings:
     HPIT_URL_ROOT = 'http://23.239.14.159:80'
     REQUESTS_LOG_LEVEL = 'debug'
 
-    def __new__(cls):
+    def __new__(cls, *args, **kwargs):
         if cls.instance:
             raise Exception("Settings has already been created.")
+
+        cls.instance = object.__new__(cls)
+        cls.instance.__init__(*args, **kwargs)
+        return cls.instance
 
     @classmethod
     def settings(cls):
