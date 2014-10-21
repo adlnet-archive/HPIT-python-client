d191ae037ba8e35bb82d6c40ecbc208c006823e7
Raymond Chandler III <raymondchandleriii@gmail.com>
2014-10-20 20:10:08 -0400

    Changed default poll_wait to 100ms

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ classifiers=[
     ]
 
 setup(name='hpitclient',
-    version='0.31',
+    version='0.32',
     description='Python Client Libraries for HPIT.',
     author='TutorGen, Inc.',
     author_email='rchandler@tutorgen.com',
